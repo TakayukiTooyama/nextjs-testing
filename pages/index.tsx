aeae093e7c9a792cec2967684afa093b9b1b8bc3
TakayukiTooyama <t.tooyama107@gmail.com>
2021-04-18 15:49:21 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+
+const Home: FC = () => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen font-mono">
+      Hello Nextjs
+    </div>
+  )
+}
+export default Home
